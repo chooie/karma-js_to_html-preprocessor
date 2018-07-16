a0fa04f618eb82583b5a25dbc322e32290db0b5c
chooie <charlie.hebert92@gmail.com>
2018-07-16 13:08:54 -0600

    Log the content the preprocessor receives

---
+++ src/application/shared/main.js
@@ -0,0 +1,19 @@
+// const jsToHtml = require("@chooie/js_to_html");
+
+function createJsToHtmlPreprocessor(logger, basePath, args, config) {
+  const log = logger.create("preprocessor.js");
+
+  return function(content, file, done) {
+    log.debug("Procesing '%s'.", file.originalPath);
+    file.path = file.originalPath.replace(/\.js$/, ".html");
+    log.debug("Content '%s'.", content);
+    done(content);
+  };
+}
+
+createJsToHtmlPreprocessor.$inject = ["logger", "config.basePath", "args", "config.pugPreprocessor"];
+
+// PUBLISH DI MODULE
+module.exports = {
+  "preprocessor:pug": ["factory", createJsToHtmlPreprocessor]
+};
